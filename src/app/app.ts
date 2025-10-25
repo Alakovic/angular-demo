f9a27f5b379781f14435ad3545a92bdd7a64fad7
Zeljko Alakovic <zeljkoalakovic997@outlook.com>
2025-10-25 16:54:27 +0200

    Added changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainContent } from './main-content/main-content';
+import { Footer } from './shared/components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule,MainContent],
+  imports: [RouterOutlet,CommonModule,Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
